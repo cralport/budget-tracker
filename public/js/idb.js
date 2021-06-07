733ed5886bbad11cf428fcfb045bfbd48b9a3873
cory alport <cralport@gmail.com>
2021-06-07 04:21:02 -0600

    working on idb page

--- public/js/idb.js
+++ public/js/idb.js
@@ -27,5 +27,40 @@ function saveRecord(record) {
 };
 
 function uploadTransaction() {
-    const transaction = db.transaction(['new_transaction'])
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+    const budgetObjectStore = transaction.objectStore('new_transaction');
+
+    const getAll = budgetObjectStore.getAll();
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0 ) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+                    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+                    const budgetObjectStore = transaction.objectStore('new_transaction');
+
+                    budgetObjectStore.clear();
+
+                    alert('All saved transactions have been submitted');
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    }
 }
+
+window.addEventListener('online', uploadTransaction);
